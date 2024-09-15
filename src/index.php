e7cc8cd4a8e8e64b75c985f480d23e82e31ad7b9
new <fir3@gmail.com>
2024-09-15 13:51:06 +0530

    change svc

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php 
 // Database connection
-$connection = mysqli_connect('10.108.23.247', 'root', 'rootpassword', 'mydb');
+$connection = mysqli_connect('10.96.104.194', 'root', 'rootpassword', 'mydb');
 
 // Check connection
 if (!$connection) {
