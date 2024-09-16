08438bbdb5a06ec76e0c46c3326fc86b26949b35
new <fir3@gmail.com>
2024-09-16 10:50:36 +0530

    update index

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php 
 // Database connection
-$connection = mysqli_connect('10.96.200.23', 'root', 'rootpassword', 'timeseries_schema');
+$connection = mysqli_connect('10.96.200.23', 'root', 'rootpassword', 'mydb');
 
 // Check connection
 if (!$connection) {
@@ -10,75 +10,75 @@ if (!$connection) {
 $id = '';
 
 // Handle form submissions
-if (isset($_POST['AAPL']) && empty($_POST['id'])) {
-    $AAPL = $_POST['AAPL'];
-    if (!empty($AAPL)) {
-        if (insertAAPL($AAPL, $connection)) {
-            echo "<script>alert('AAPL Inserted');</script>";
+if (isset($_POST['todo']) && empty($_POST['id'])) {
+    $todo = $_POST['todo'];
+    if (!empty($todo)) {
+        if (insertTodo($todo, $connection)) {
+            echo "<script>alert('Todo Inserted');</script>";
         } else {
-            echo "<script>alert('Error inserting AAPL');</script>";
+            echo "<script>alert('Error inserting Todo');</script>";
         }
     } else {
-        echo "<script>alert('Please enter a AAPL');</script>";
+        echo "<script>alert('Please enter a Todo');</script>";
     }
 }
 
-if (isset($_POST['AAPL']) && !empty($_POST['id'])) {
+if (isset($_POST['todo']) && !empty($_POST['id'])) {
     $id = $_POST['id'];
-    $AAPL = $_POST['AAPL'];
-    if (updateAAPL($id, $AAPL, $connection)) {
-        echo "<script>alert('AAPL Updated');</script>";
+    $todo = $_POST['todo'];
+    if (updateTodo($id, $todo, $connection)) {
+        echo "<script>alert('Todo Updated');</script>";
     } else {
-        echo "<script>alert('Error updating AAPL');</script>";
+        echo "<script>alert('Error updating Todo');</script>";
     }
 }
 
 if (isset($_GET['delete_id'])) {
     $id = $_GET['delete_id'];
-    if (deleteAAPL($id, $connection)) {
-        echo "<script>alert('AAPL Deleted');</script>";
+    if (deleteTodo($id, $connection)) {
+        echo "<script>alert('Todo Deleted');</script>";
     } else {
-        echo "<script>alert('Error deleting AAPL');</script>";
+        echo "<script>alert('Error deleting Todo');</script>";
     }
 }
 
 if (isset($_GET['marking_id'])) {
     $id = $_GET['marking_id'];
-    if (completeAAPL($id, $connection)) {
-        echo "<script>alert('AAPL marked as complete');</script>";
+    if (completeTodo($id, $connection)) {
+        echo "<script>alert('Todo marked as complete');</script>";
     } else {
-        echo "<script>alert('Error marking AAPL as complete');</script>";
+        echo "<script>alert('Error marking Todo as complete');</script>";
     }
 }
 
-function insertAAPL($AAPL, $connection) {
-    $query = "INSERT INTO AAPLs (AAPL) VALUES ('$AAPL')";
+function insertTodo($todo, $connection) {
+    $query = "INSERT INTO todos (todo) VALUES ('$todo')";
     return mysqli_query($connection, $query);
 }
 
-function deleteAAPL($id, $connection) {
-    $query = "DELETE FROM AAPLs WHERE id = '$id'";
+function deleteTodo($id, $connection) {
+    $query = "DELETE FROM todos WHERE id = '$id'";
     return mysqli_query($connection, $query);
 }
 
-function completeAAPL($id, $connection) {
-    $query = "UPDATE AAPLs SET completed = 1 WHERE id = '$id'";
+function completeTodo($id, $connection) {
+    $query = "UPDATE todos SET completed = 1 WHERE id = '$id'";
     return mysqli_query($connection, $query);
 }
 
 if (isset($_POST['update'])) {
     $id = $_POST['update_id'];
-    $data = getAAPL($id, $connection);
+    $data = getTodo($id, $connection);
 }
 
-function getAAPL($id, $connection) {
-    $query = "SELECT * FROM AAPLs WHERE id = '$id'";
+function getTodo($id, $connection) {
+    $query = "SELECT * FROM todos WHERE id = '$id'";
     $result = mysqli_query($connection, $query);
     return $result ? mysqli_fetch_assoc($result) : false;
 }
 
-function updateAAPL($id, $AAPL, $connection) {
-    $query = "UPDATE AAPLs SET AAPL = '$AAPL' WHERE id = '$id'";
+function updateTodo($id, $todo, $connection) {
+    $query = "UPDATE todos SET todo = '$todo' WHERE id = '$id'";
     return mysqli_query($connection, $query);
 }
 ?>
@@ -86,7 +86,7 @@ function updateAAPL($id, $AAPL, $connection) {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>3-Tier PHP AAPL App</title>
+    <title>3-Tier PHP Todo App</title>
     <style type="text/css">
         body {
             background-color: #f0f8ff; /* Light blue background */
@@ -138,11 +138,11 @@ function updateAAPL($id, $AAPL, $connection) {
             text-decoration: none;
             color: #3e3e3e;
         }
-        .AAPL-item {
+        .todo-item {
             padding: 10px 0;
             border-bottom: 1px solid #ddd;
         }
-        .AAPL-item:last-child {
+        .todo-item:last-child {
             border-bottom: none;
         }
         hr {
@@ -169,12 +169,12 @@ function updateAAPL($id, $AAPL, $connection) {
     </style>
 </head>
 <body>
-    <div class="header">Welcome to SenDevOps - 3-Tier PHP AAPL App</div>
+    <div class="header">Welcome to SenDevOps - 3-Tier PHP Todo App</div>
     <div class="wrapper">
         <form method="post" action="index.php">
             <div>
                 <center>
-                    <input type="text" name="AAPL" placeholder="Create new AAPL" value="<?php if (isset($_POST['update_id'])) { echo htmlspecialchars($data['AAPL']); } ?>" required>
+                    <input type="text" name="todo" placeholder="Create new todo" value="<?php if (isset($_POST['update_id'])) { echo htmlspecialchars($data['todo']); } ?>" required>
                     <input type="hidden" name="id" value="<?php if (isset($_POST['update_id'])) { echo htmlspecialchars($data['id']); } ?>">
                     <input class="button" type="submit" value="Submit">
                 </center>
@@ -183,22 +183,22 @@ function updateAAPL($id, $AAPL, $connection) {
         <br><br>
 
         <?php 
-        $query = "SELECT * FROM AAPLs ORDER BY id DESC";
+        $query = "SELECT * FROM todos ORDER BY id DESC";
         $result = mysqli_query($connection, $query);
         $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        foreach ($row as $AAPLs) { ?>
-            <div class="AAPL-item">
+        foreach ($row as $todos) { ?>
+            <div class="todo-item">
                 <center>
-                    <?php echo htmlspecialchars($AAPLs['AAPL']); ?>
-                    <button><a href='index.php?delete_id=<?php echo htmlspecialchars($AAPLs['id']); ?>'>Delete</a></button>
+                    <?php echo htmlspecialchars($todos['todo']); ?>
+                    <button><a href='index.php?delete_id=<?php echo htmlspecialchars($todos['id']); ?>'>Delete</a></button>
                     <form class="form" method="post" action="">
-                        <input type="hidden" name="update_id" value="<?php echo htmlspecialchars($AAPLs['id']); ?>">
+                        <input type="hidden" name="update_id" value="<?php echo htmlspecialchars($todos['id']); ?>">
                         <input type="submit" value="Update" name="update">
                     </form>
-                    <?php if ($AAPLs['completed'] == 1) {
+                    <?php if ($todos['completed'] == 1) {
                         echo "Completed";
                     } else { ?>
-                        <button><a href='index.php?marking_id=<?php echo htmlspecialchars($AAPLs['id']); ?>'>Mark complete</a></button>
+                        <button><a href='index.php?marking_id=<?php echo htmlspecialchars($todos['id']); ?>'>Mark complete</a></button>
                     <?php } ?>
                 </center>
             </div>
